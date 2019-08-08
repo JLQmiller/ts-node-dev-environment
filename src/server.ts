d1d9add84b0e92d6d680f4f792b145935fb32749
JLQmiller <894447836@qq.com>
2019-08-08 16:44:19 +0800

    debug/correct tslint config

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,123 @@
-// import errorHandler from "errorhandler";
-
-// import app from "./app";
-
-// /**
-//  * Error Handler. Provides full stack - remove for production
-//  */
-// app.use(errorHandler());
-
-// /**
-//  * Start Express server.
-//  */
-// const server = app.listen(app.get("port"), () => {
-//     console.log(
-//         "  App is running at http://localhost:%d in %s mode",
-//         app.get("port"),
-//         app.get("env")
-//     );
-//     console.log("  Press CTRL-C to stop\n");
-// });
-
-// export default server;
-
 export function hello(): number {
-    console.log("fuck");
-    return 5;
+  console.log("fuck");
+  return 5;
 }
 
 hello();
 
-class Province {
-    constructor(doc) {
-        this._name = doc.name;
-        th;
-    }
+export function sampleProvinceData() {
+  return {
+    name: "Asia",
+    producers: [
+      { name: "By", cost: 10, production: 9 },
+      { name: "Attalia", cost: 12, production: 1 },
+      { name: "Sinope", cost: 10, production: 6 },
+    ],
+    demand: 30,
+    price: 20
+  };
 }
+
+export class Province {
+  private _name: string;
+  private _producers: any[];
+  private _totalProduction: number;
+  private _demand: number;
+  private _price: number;
+  constructor(doc: any) {
+    this._name = doc.name;
+    this._producers = [];
+    this._totalProduction = 0;
+    this._demand = doc.demand;
+    this._price = doc.price;
+    doc.producers.forEach((d: any) => this.addProducer(new Producer(this, d)));
+  }
+  public get name() {
+    return this._name;
+  }
+  public get producers() {
+    return this._producers.slice();
+  }
+  public get totalProduction() {
+    return this._totalProduction;
+  }
+  public set totalProduction(arg) {
+    this._totalProduction = arg;
+  }
+  public get demand() {
+    return this._demand;
+  }
+  public set demand(arg) {
+    this._demand = arg;
+  }
+  public get price() {
+    return this._price;
+  }
+  public set price(arg) {
+    this._price = arg;
+  }
+  public get shortfall(): number {
+    return this._demand - this.totalProduction;
+  }
+  public get profit() {
+    return (this.demandValue - this.demandCost) as number;
+  }
+  public get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach(p => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
+  private get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+  private get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+  private addProducer(arg: any) {
+    this._producers.push(arg);
+    this._totalProduction += arg.production;
+  }
+}
+
+export class Producer {
+  private _province: Province;
+  private _cost: number;
+  private _name: string;
+  private _production: any;
+  constructor(aProvince: Province, data: any) {
+    this._province = aProvince;
+    this._cost = data.cost;
+    this._name = data.name;
+    this._production = data.production || 0;
+  }
+  public get name() {
+    return this._name;
+  }
+  public get cost() {
+    return this._cost;
+  }
+  public set cost(arg) {
+    this._cost = parseInt(arg.toString());
+  }
+  public get production() {
+    return this._production;
+  }
+  public set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this._production.totalProduction += newProduction - this._production;
+    this._production = newProduction;
+  }
+}
+
+const newInstance = new Province(sampleProvinceData());
+console.log(`console.log:  ${newInstance.name}`);
+newInstance.totalProduction = 3;
+console.log(`console.log:  ${newInstance.totalProduction}`);
--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,6 +1,47 @@
 import { expect } from "chai";
-import { hello } from "../src/server";
+import {
+  hello,
+  Producer,
+  Province,
+  sampleProvinceData,
+} from "../src/server";
 
-test("it should hello", () => {
+describe("Province", () => {
+  let asia: Province;
+  beforeEach(function() {
+    asia = new Province(sampleProvinceData());
+  });
+
+  test("it should hello", () => {
     expect(hello()).to.equal(5);
+  });
+  
+  test("it should province profit", () => {
+    expect(asia.profit).to.equal(158);
+  });
+  
+  test("it should province shortfall", () => {
+    expect(asia.shortfall).to.equal(14);
+  });
+  
+  test("it should province name", () => {
+    expect(asia.name).to.equal("Asia");
+  });
+
+  test("it should province set totalProduction", () => {
+    asia.totalProduction = 3;
+    expect(asia.totalProduction).to.equal(3);
+  });
+
+  test("it should province set demand", () => {
+    asia.demand = 2;
+    expect(asia.demand).to.equal(2);
+  });
 });
+
+// describe("Producer", () => {
+//   let producer: Producer;
+//   beforeEach(function() {
+//     producer = new Producer(new Province(sampleProvinceData()), );
+//   })
+// })
